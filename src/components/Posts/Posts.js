9eda9f600607ab555e9707b0c7eca63e5c792272
kate roy <kroy15chs@gmail.com>
2020-07-14 20:52:47 -0400

    MVP met

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -4,12 +4,20 @@ import "./Posts.css";
 
 const Posts = (props) => {
   // Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
+  const { posts, likePost} = props;
+  // console.log('props working!!', props)
 
   return (
     <div className="posts-container-wrapper">
-      {/* map through the posts here to return a Post component */}
-      {/* Check the implementation of Post to see what props it requires! */}
+      {
+        posts.map((post) => {
+          return <Post 
+          key = {post.id}
+          post = {post}
+          likePost={likePost}
+          />
+        })
+      }
     </div>
   );
 };
